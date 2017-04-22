d0584dbf180628283c74013fbf60ceab08ad1dc8
Jonathan Roeber <r734@fastmail.com>
2017-04-22 12:08:53 -0400

    Document Note, BasicNote, and BasicScale

--- include/BasicNote.h
+++ include/BasicNote.h
@@ -1,15 +1,19 @@
+/*! \file BasicNote.h
+A scoped enum for basic note names. */
+
 // BasicNote.h
 #ifndef BASICNOTE_H
 #define BASICNOTE_H
 
+/*! A scoped enum containing the seven basic note names. */
 enum class BasicNote {
-	A,
-	B,
-	C,
-	D,
-	E,
-	F,
-	G
+	A /*! The note A */,
+	B /*! The note B */,
+	C /*! The note C */,
+	D /*! The note D */,
+	E /*! The note E */,
+	F /*! The note F */,
+	G /*! The note G */
 };
 
 #endif // BASICNOTE_H
--- include/BasicScale.h
+++ include/BasicScale.h
@@ -1,21 +1,26 @@
+/*! \file BasicScale.h
+A scoped enum for basic diatonic scale names. */
+
 // BasicScale.h
 #ifndef BASICSCALE_H
 #define BASICSCALE_H
 
 /*! The BasicScale enum provides commonly-recognized diatonic scales for use as input to the DiatonicScale constructor. */
 enum class BasicScale {
-	Chromatic,
-	Major,
-	Minor, // i.e. natural minor
+	
+	Major			/*! The major scale; intervals P1 M2 M3 P4 P5 M6 M7 */,
+	Minor			/*! The natural minor scale; intervals P1 M2 m3 P4 P5 m6 m7 */,
+	HarmonicMinor	/*! The harmonic minor scale; intervals P1 M2 m3 P4 P5 m6 M7 */,
+	Ionian			/*! The Ionian mode, also known as the major scale; intervals P1 M2 M3 P4 P5 M6 M7 */,
+	Dorian			/*! The Dorian mode; intervals P1 M2 m3 P4 P5 M6 m7 */,
+	Phrygian		/*! The Phrygian mode; intervals P1 m2 m3 P4 P5 m6 m7 */,
+	Lydian			/*! The Lydian mode; intervals P1 M2 M3 A4 P5 M6 M7 */,
+	Mixolydian		/*! The Mixolydian mode; intervals P1 M2 M3 P4 P5 M6 m7 */,
+	Aeolian			/*! The Aeolian mode, also known as the natural minor scale; intervals P1 M2 m3 P4 P5 m6 m7 */,
+	Locrian			/*! The Locrian mode; intervals P1 m2 m3 P4 d5 m6 m7 */
+
+	// Chromatic, // TODO should non-diatonic scales go in this enum or some other one?
 	// MelodicMinor, // TODO figure out how to deal with this one
-	HarmonicMinor,
-	Ionian, // same as Major
-	Dorian,
-	Phrygian,
-	Lydian,
-	Mixolydian,
-	Aeolian, // same as Minor
-	Locrian
 };
 
 #endif // BASICSCALE_H
